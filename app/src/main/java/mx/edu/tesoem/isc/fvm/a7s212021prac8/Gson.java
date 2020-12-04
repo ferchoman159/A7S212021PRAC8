0fcc13212d75fbdac5f23c38d3fde59bd018eca7
Fernando Villanueva Mayorga <71373619+ferchoman159@users.noreply.github.com>
2020-12-04 03:46:11 -0600

    Initial commit

---
+++ app/src/main/java/mx/edu/tesoem/isc/fvm/a7s212021prac8/Gson.java
@@ -0,0 +1,9 @@
+package mx.edu.tesoem.isc.fvm.a7s212021prac8;
+
+public class Gson {
+    public Datos fromJson(String toString, Class<Datos> datosClass) {
+    }
+
+    public int toJson(Datos dato) {
+    }
+}
